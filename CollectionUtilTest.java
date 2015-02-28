58678c157b5f7f74006574f833e465b162262e34
Kittu Jain <cskratijain@gmail.com>
2015-02-28 12:05:55 +0530

    version 1 completed filter done with string

--- CollectionUtilTest.java
+++ CollectionUtilTest.java
@@ -7,14 +7,20 @@ interface ListFilter<E> {
 	boolean filterMethod(E element);
 }
 
+interface ListMapper<E> {
+	E mapMethod(E element);
+}
+
 class IntegerFilter implements ListFilter<Integer> {
 	public boolean filterMethod (Integer element){
 		return (element > 2);
 	}
 }
 
-interface ListMapper<E> {
-	E mapMethod(E element);
+class StringFilter implements ListFilter<String> {
+	public boolean filterMethod (String element){
+		return (element == "yellow");
+	}
 }
 
 class IntegerMapper implements ListMapper<Integer> {
@@ -26,7 +32,7 @@ class IntegerMapper implements ListMapper<Integer> {
 public class CollectionUtilTest {
 	@Test
 	public void filter_returns_an_integer_List_of_elements_that_are_greater_than_2(){
-		ListFilter listFilter = new IntegerFilter();
+		ListFilter<Integer> listFilter = new IntegerFilter();
 		List<Integer> numbers = new ArrayList<Integer>();
 		List<Integer> expected = new ArrayList<Integer>();
 		
@@ -36,9 +42,24 @@ public class CollectionUtilTest {
 		expected.add(6);
 		expected.add(9);
 
-		List<Integer> filteredList = CollectionUtil.<Integer>filter(numbers,listFilter);
+		List<Integer> filteredList = CollectionUtil.<Integer>filter(numbers, listFilter);
 		assertEquals(expected.get(0), filteredList.get(0));
 		assertEquals(expected.get(1), filteredList.get(1));
+	}
+
+	@Test
+	public void filter_returns_a_string_List_of_elements_that_are_yellow(){
+		ListFilter<String> listFilter = new StringFilter();
+		List<String> colors = new ArrayList<String>();
+		List<String> expected = new ArrayList<String>();
+		
+		colors.add("red");
+		colors.add("blue");
+		colors.add("yellow");
+		expected.add("yellow");
+
+		List<String> filteredList = CollectionUtil.<String>filter(colors, listFilter);
+		assertEquals(expected.get(0), filteredList.get(0));
 	}
 
 	@Test
@@ -56,8 +77,8 @@ public class CollectionUtilTest {
 		expected.add(4);
 
 		List<Integer> mappedList = CollectionUtil.<Integer>map(numbers,listMapper);
-		assertEquals(expected.get(0),mappedList.get(0));
-		assertEquals(expected.get(1),mappedList.get(1));
-		assertEquals(expected.get(2),mappedList.get(2));
+		assertEquals(expected.get(0), mappedList.get(0));
+		assertEquals(expected.get(1), mappedList.get(1));
+		assertEquals(expected.get(2), mappedList.get(2));
 	}
 }
